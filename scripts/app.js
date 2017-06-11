c74066f5c41852a1e9e9aa0220151fc8e7555ac1
iddiaz <diazdiazivan@gmail.com>
2017-06-11 23:10:15 +0200

    Implementación de cards

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,12 @@
 angular.module('ngClassifields', ['ngMaterial'])
    .config(function($mdThemingProvider){
       $mdThemingProvider.theme('default')
-      .primaryPalette('teal')
+      .primaryPalette('green')
       .accentPalette('orange')
+   })
+   .directive('helloWorld', function(){
+      return {
+         // template: '<h1>Hello, World</h1>'
+         template: '<h1>{{message}}</h1>'
+      }
    })

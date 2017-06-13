c2757d8c263ce15f7feb8fb16a52b24fe9bb6b79
iddiaz <diazdiazivan@gmail.com>
2017-06-13 13:36:53 +0200

    Implementación de ui-router básica

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,21 @@
-angular.module('ngClassifields', ['ngMaterial'])
-   .config(function($mdThemingProvider){
+angular.module('ngClassifields', ['ngMaterial', 'ui.router'])
+   .config(function($mdThemingProvider, $stateProvider){
       $mdThemingProvider.theme('default')
       // .dark();
       // .primaryPalette('green')
       // .accentPalette('orange')
+
+      //ROUTING CONFIG
+      $stateProvider
+         .state('stateone', {
+            url: '/stateone',
+            template: '<h1>State One</h1>'
+         })
+         .state('statetwo', {
+            url: '/statetwo',
+            template: '<h1>State Two</h1>'
+         })
+
    })
    .directive('helloWorld', function(){
       return {
